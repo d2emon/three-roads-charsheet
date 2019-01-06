ff402c25aea4329a9de58055b6e94377db9e09a7
Dmitry Kutsenko <d2emonium@gmail.com>
2019-01-06 21:34:23 +0400

    Game spells

---
+++ src/components/GameSpells.vue
@@ -0,0 +1,64 @@
+<template>
+    <v-card>
+        <InputBox
+                input-box-id="spell"
+                title="Spell Name"
+                label="Enter Spell"
+                default="Spell"
+                @input="addSpell"
+        />
+
+        <v-toolbar>
+            <v-toolbar-title>Заклинания</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items class="hidden-sm-and-down">
+                <v-btn icon flat @click="showInput('spell')"><v-icon>add</v-icon></v-btn>
+                <v-btn icon flat @click="resetSpells"><v-icon>clear</v-icon></v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+        <v-card-text>
+            <v-list>
+                <v-list-tile
+                        v-for="(item, index) in spells"
+                        :key="index"
+                >
+                    <v-list-tile-content>
+                        <v-list-tile-title v-html="item"></v-list-tile-title>
+                    </v-list-tile-content>
+                    <v-list-tile-action>
+                        <v-btn icon flat @click="deleteSpell(index)"><v-icon>remove</v-icon></v-btn>
+                    </v-list-tile-action>
+                </v-list-tile>
+            </v-list>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+    import { mapState, mapMutations } from 'vuex'
+    import InputBox from './InputBox'
+
+    export default {
+        name: 'GameSpells',
+        components: {
+            InputBox,
+        },
+        computed: {
+            ...mapState([
+                'spells'
+            ]),
+        },
+        methods: {
+            ...mapMutations( 'input', [ 'showInput', ] ),
+            ...mapMutations([
+                'addSpell',
+                'deleteSpell',
+                'resetSpells',
+            ])
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -8,14 +8,6 @@
         @input="setPlayerName"
     />
 
-    <InputBox
-        input-box-id="spell"
-        title="Spell Name"
-        label="Enter Spell"
-        default="Spell"
-        @input="addSpell"
-    />
-
     <TestResult
         title="Результат"
     />
@@ -135,31 +127,7 @@
               <GamePoints />
             </v-flex>
             <v-flex xs6>
-              <v-card>
-                <v-toolbar>
-                  <v-toolbar-title>Заклинания</v-toolbar-title>
-                  <v-spacer></v-spacer>
-                  <v-toolbar-items class="hidden-sm-and-down">
-                    <v-btn icon flat @click="showInput('spell')"><v-icon>add</v-icon></v-btn>
-                    <v-btn icon flat @click="clearSpells"><v-icon>clear</v-icon></v-btn>
-                  </v-toolbar-items>
-                </v-toolbar>
-                <v-card-text>
-                  <v-list>
-                    <v-list-tile
-                      v-for="(item, index) in spells"
-                      :key="index"
-                    >
-                      <v-list-tile-content>
-                        <v-list-tile-title v-html="item"></v-list-tile-title>
-                      </v-list-tile-content>
-                      <v-list-tile-action>
-                        <v-btn icon flat @click="deleteSpell(index)"><v-icon>remove</v-icon></v-btn>
-                      </v-list-tile-action>
-                    </v-list-tile>
-                  </v-list>
-                </v-card-text>
-              </v-card>
+              <GameSpells />
             </v-flex>
 
             <v-flex xs12>
@@ -186,10 +154,12 @@
   import GameTime from './GameTime'
   import GameItems from './GameItems'
   import GamePoints from "./GamePoints";
+  import GameSpells from "./GameSpells";
 
   export default {
     name: "Main",
     components: {
+        GameSpells,
         GamePoints,
         InputBox,
         TestResult,
@@ -213,10 +183,6 @@
       ],
     }),
     methods: {
-      addSpell(name) { this.$store.commit('addSpell', name) },
-      deleteSpell(index) { this.$store.commit('deleteSpell', index) },
-      clearSpells() { this.$store.commit('resetSpells') },
-
       resetPlayer() { this.$store.dispatch('reset') },
 
       savePlayer() { /* procedure Save1Click(Sender: TObject); */ },
